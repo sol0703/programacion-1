f2760b157e3a22d2c77f396fb272d4d13b96a7de
sol0703 <saralarrea07@gmail.com>
2021-02-11 19:30:15 -0500

    subir clase 2

---
+++ clases/clase2.py
@@ -0,0 +1,27 @@
+#Estos son booleans que son variables que solo vale
+# verdadero o falso
+pruebaV = True
+pruebaF = False
+print (pruebaV)
+print (pruebaF)
+pruebaV = pruebaF
+print (pruebaV)
+edad = 19
+estatura = 1.63
+peso = 65
+NOMBRE = "Sara Osorno"
+print ("#"*15,"Mayor Edad", "#"*15)
+isMayorEdad = edad >= 18
+print (isMayorEdad)
+print ("#"*15, "Bajo la Estatura promedio", "#"*15)
+isMayorEstatura = estatura < 1.70
+print (isMayorEstatura)
+# Calculando si el peso es diferente de 84
+print ("#"*15, "Peso diferente 65",  "#"*15)
+isPesoDiferente = peso != 65
+print (isPesoDiferente)
+# vamos a ver si un apellido esta en el nombre completo
+apellido = "Osorno"
+isApellido = apellido in NOMBRE
+print ("#"*15, "Eata el apellido en el nombre?", "#"*15)
+print (isApellido)
---
+++ clases/operaciones.py
@@ -0,0 +1,13 @@
+numeroA=87
+numeroB=83
+sumar=numeroA+numeroB
+print("el resultado de la suma es", sumar)
+print(f"la suma dio {sumar} exitosamente")
+restar=numeroA-numeroB
+print(f"la resta dio {restar} exitosamente")
+multiplicar=numeroA*numeroB
+print(f"la multiplicar dio {multiplicar} exitosamente")
+dividir=numeroA/numeroB
+print(f"la dividir dio {dividir} exitosamente")
+exponente=numeroA**numeroB
+print(f"la exponente dio {exponente} exitosamente")
