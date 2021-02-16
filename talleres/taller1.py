a9cbbf981d7599f3677a54293f8a90e7d6500044
sol0703 <saralarrea07@gmail.com>
2021-02-15 19:06:49 -0500

    el primer taller

---
+++ talleres/taller1.py
@@ -0,0 +1,18 @@
+# HACIENDO POR PRIMERA VEZ IGUALDAD
+numeroQ = 45
+numeroW = 45
+igualdad = numeroQ = numeroW
+print (f"la igualdad dio {igualdad} exitosamente")
+# PRACTICA DE LAS OPERACIONES
+numeroP = 23
+numeroL = 76
+sumar=numeroP+numeroL
+print(f"la suma dio {sumar} exitosamente")
+restar=numeroP-numeroL
+print(f"la resta dio {restar} exitosamente")
+multiplicar=numeroP*numeroL
+print(f"la multiplicar dio {multiplicar} exitosamente")
+dividir=numeroP/numeroL
+print(f"la dividir dio {dividir} exitosamente")
+exponente=numeroP**numeroL
+print(f"la exponente dio {exponente} exitosamente")
