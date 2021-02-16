22676804d0cfbd5d6ea95a0f44b5a958e5f51832
sol0703 <saralarrea07@gmail.com>
2021-02-16 07:28:24 -0500

    subiendo clases

---
+++ clases/inputsclase.py
@@ -0,0 +1,11 @@
+#----Constante----#
+PREGUNTA_NOMBRE = "Como te llamas? : "
+PREGUNTA_EDAD = "Cuantos años tienes? : "
+PREGUNTA_ESTATURA = "Cuantos metros mides? : "
+MENSAJE_SALUDO = "Un gusto conocerte"
+
+# Entrada al codigo
+nombre = input (PREGUNTA_NOMBRE)
+print (MENSAJE_SALUDO, nombre)
+edad = int (input (PREGUNTA_EDAD))
+estatura = float (input(PREGUNTA_ESTATURA))
