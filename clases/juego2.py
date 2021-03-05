b6530748693692dee1d88f5e367c9ca6a887820d
sol0703 <saralarrea07@gmail.com>
2021-03-04 19:20:34 -0500

    continuacion

--- clases/juego2.py
+++ clases/juego2.py
@@ -5,6 +5,9 @@ MENSAJE_SALUDO = '''
     Bienvenido
         a este programa,
     !!!juguemos!!'''
+MENSAJE_SEGUDO_NIVEL = 'Felicidades pasaste el primer nivel ahora ve por el ultimo!!'
+MENSAJE_CALIENTE = 'Estas caliente'
+MENSAJE_FRIO = 'Estas frio'
 PREGUNTAR_NUMERO  = '''
         En este juego debes acertar un numero entero 
         que va desde el 1-10, la idea es que 
@@ -23,6 +26,7 @@ MENSAJE_PERDISTE  =  'Perdiste, vuelve a intentarlo !!'
 
 # Entrada al codigo
 NumeroOculto = random.randint(1,10)
+NueroOcultoDos = random.randint(1,10)
 vidas = None
 
 dificultad = int (input (PREGUNTA_DIFICULTAD))
@@ -32,22 +36,42 @@ while (dificultad !=1 and dificultad !=2 and dificultad !=3):
 
 if (dificultad ==1):
     print ('Modo facil activado')
-    vidas = 5
+    vidas = 10
 elif (dificultad == 2):
     print ('Modo moderado activado')
-    vidas = 3
+    vidas = 5
 else:
     print ('Modo dificil activado, sssssss mucho cuidado')
-    vidas = 1
+    vidas = 2
 
 NumeroIngresado = int (input(PREGUNTAR_NUMERO))
 while (NumeroIngresado != NumeroOculto and vidas>1):
+    if (NumeroIngresado > NumeroOculto):
+        print (MENSAJE_CALIENTE)
+    else:
+        print (MENSAJE_FRIO)
     vidas -=1
-    NumeroIngresado = int (input (PREGUNTA_FALLIDA))
     print (f'te quedan {vidas} vidas')
     NumeroIngresado = int (input(PREGUNTA_FALLIDA))
-
 if (vidas >=0 and NumeroIngresado == NumeroOculto):
+    print (MENSAJE_SEGUDO_NIVEL)
+    NumeroIngresado = int (input(PREGUNTAR_NUMERO))
+    while (NumeroIngresado != NueroOcultoDos and vidas>1):
+        if (NumeroIngresado > NueroOcultoDos):
+            print (MENSAJE_CALIENTE)
+        else:
+            print (MENSAJE_FRIO)
+        vidas -=1
+        print (f'te quedan {vidas} vidas')
+        NumeroIngresado = int (input (PREGUNTA_FALLIDA))
+
+
+if (vidas >=0 and NumeroIngresado == NueroOcultoDos ):
     print (MENSAJE_GANASTE)
+
 else:
-    print (MENSAJE_PERDISTE, 'El numero era', NumeroOculto)
+    print (MENSAJE_PERDISTE,
+        'El numero era el',
+        NumeroOculto,
+        'El numero dos era el',
+        NueroOcultoDos)
