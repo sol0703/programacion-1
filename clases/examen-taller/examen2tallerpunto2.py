cfc964e4d41edea6b6ddc50d63f5ed9b6cbdf4e9
sol0703 <saralarrea07@gmail.com>
2021-03-15 23:57:59 -0500

    clases de taller y lista

---
+++ clases/examen-taller/examen2tallerpunto2.py
@@ -0,0 +1,6 @@
+listaPesos = [20000,30000,4000,2500,1000,7600]
+preguntarNumero = 'Ingrese un valor COP:'
+valorIngresado = float (input(preguntarNumero))
+listaPesos.append(valorIngresado)
+print (listaPesos)
+
